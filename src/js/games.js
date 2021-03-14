c7cb2dddd1f5d420e34b9ed65b41b1ec142e4f7d
nafifurqon <muhammadnafi98@gmail.com>
2021-03-14 18:13:54 +0700

    get computer choice using Math.random() and print the winner to the console

--- src/js/games.js
+++ src/js/games.js
@@ -2,9 +2,57 @@ const playerChoice = document.getElementsByClassName('player-choice-box');
 // console.log(playerChoice)
 // console.log(playerChoice[0].childNodes[1]);
 
+function comChooses() {
+    const arrayComChoice = ["rock", "paper", "scissors"];
+    const choice = arrayComChoice[Math.floor(Math.random()*3)];
+    document.querySelector(`.com-choice-box#${choice}`).classList.add('active');
+
+    return choice;
+}
+
+// console.log(document.querySelector(`.com-choice-box#${comChoice}`))
+
 const action = (e) => {
     const event = e.target || e.srcElement;
-    const choice = event.id;    
+    const playerChoice = event.id;
+
+    const comChoice = comChooses();
+    console.log("Player 1:", playerChoice);
+    console.log("Com:", comChoice);
+
+    if(playerChoice == comChoice){
+        console.log("DRAW");
+    } 
+
+    if(playerChoice == "rock"){
+        if(comChoice == "paper"){
+            console.log("COM WIN");
+        }
+
+        if(comChoice == "scissors"){
+            console.log("PLAYER 1 WIN")
+        }
+    }
+
+    if(playerChoice == "paper"){
+        if(comChoice == "rock"){
+            console.log("PLAYER 1 WIN");
+        }
+
+        if(comChoice == "scissors"){
+            console.log("COM WIN")
+        }
+    }
+
+    if(playerChoice == "scissors"){
+        if(comChoice == "rock"){
+            console.log("COM WIN");
+        }
+
+        if(comChoice == "paper"){
+            console.log("PLAYER 1 WIN")
+        }
+    }
 }
 
 for(let i=0; i < playerChoice.length; i++){
