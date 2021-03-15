cb867fda03acfecc8963c76387824c63bc7d3d88
nafifurqon <muhammadnafi98@gmail.com>
2021-03-15 14:39:22 +0700

    finished function to show the result and refresh to play again

--- src/js/games.js
+++ src/js/games.js
@@ -1,7 +1,6 @@
 const playerChoice = document.getElementsByClassName('player-choice-box');
-// console.log(playerChoice)
-// console.log(playerChoice[0].childNodes[1]);
 let comChoice;
+const resultBody = document.getElementById('result-body');
 
 function comChooses() {
     const arrayComChoice = ["rock", "paper", "scissors"];
@@ -10,8 +9,6 @@ function comChooses() {
 
     return choice;
 }
-
-// console.log(document.querySelector(`.com-choice-box#${comChoice}`))
 
 function chooseTheWinner(playerChoice, comChoice) {
     let result;
@@ -58,9 +55,80 @@ const action = (e) => {
     const playerChoice = event.id;
 
     comChoice = comChooses();
+    const result = chooseTheWinner(playerChoice, comChoice);
+
     console.log("Player 1:", playerChoice);
     console.log("Com:", comChoice);
-    console.log("Result:", chooseTheWinner(playerChoice, comChoice));
+    console.log("Result:", result);
+
+    if(result == "player 1"){
+        if(resultBody.className == 'versus-wrapper'){
+            resultBody.classList.replace('versus-wrapper', 'player-1-result');
+            while (resultBody.firstChild) {
+                resultBody.removeChild(resultBody.firstChild);
+            }
+        }
+
+        const player1Text = document.createElement('h2');
+        const player1WinText = document.createElement('h2');
+        
+        player1Text.classList.add('text-uppercase');
+        player1Text.classList.add('p-0');
+        player1Text.classList.add('m-0');
+        player1Text.innerHTML = 'player 1';
+        
+        player1WinText.classList.add('text-uppercase');
+        player1WinText.classList.add('p-0');
+        player1WinText.classList.add('m-0');
+        player1WinText.innerHTML = 'win';
+
+        resultBody.appendChild(player1Text);
+        resultBody.appendChild(player1WinText);
+    }
+
+    if(result == "com"){
+        if(resultBody.className == 'versus-wrapper'){
+            resultBody.classList.replace('versus-wrapper', 'com-result');
+            
+            while (resultBody.firstChild) {
+                resultBody.removeChild(resultBody.firstChild);
+            }
+        }
+
+        const comText = document.createElement('h2');
+        const comWinText = document.createElement('h2');
+        
+        comText.classList.add('text-uppercase');
+        comText.classList.add('p-0');
+        comText.classList.add('m-0');
+        comText.innerHTML = 'com';
+        
+        comWinText.classList.add('text-uppercase');
+        comWinText.classList.add('p-0');
+        comWinText.classList.add('m-0');
+        comWinText.innerHTML = 'win';
+
+        resultBody.appendChild(comText);
+        resultBody.appendChild(comWinText);
+    }
+
+    if(result == "draw"){
+        if(resultBody.className == 'versus-wrapper'){
+            resultBody.classList.replace('versus-wrapper', 'draw-result');
+            while (resultBody.firstChild) {
+                resultBody.removeChild(resultBody.firstChild);
+            }
+        }
+
+        const drawText = document.createElement('h2');
+        
+        drawText.classList.add('text-uppercase');
+        drawText.classList.add('p-0');
+        drawText.classList.add('m-0');
+        drawText.innerHTML = 'draw';
+
+        resultBody.appendChild(drawText);
+    }
 }
 
 for(let i=0; i < playerChoice.length; i++){
@@ -73,4 +141,19 @@ function refresh() {
     if(comChoice){
         document.querySelector(`.com-choice-box#${comChoice}`).classList.remove('active');
     }
+
+    while (resultBody.firstChild) {
+        resultBody.removeChild(resultBody.firstChild);
+    }
+
+    resultBody.className = '';
+    resultBody.classList.add('versus-wrapper')
+    const versusText = document.createElement('h2');
+
+    versusText.classList.add('text-uppercase');
+    versusText.classList.add('font-weight-bold');
+    versusText.classList.add('versus-text');
+    versusText.innerHTML = 'vs';
+
+    resultBody.appendChild(versusText);
 }
