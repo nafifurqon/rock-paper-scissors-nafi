292098abac6c31c903e5ed0a2a9ea2f298b6ed3e
nafifurqon <muhammadnafi98@gmail.com>
2021-04-10 13:52:00 +0700

    changed index router to become games router

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const indexRouter = require('./routes/index')
+const gamesRouter = require('./routes/games')
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
-app.use('/', indexRouter);
+app.use('/', gamesRouter);
 app.use(express.static('static'))
 
 app.listen(port, () => console.log(`Binar challenge app listening at http://localhost:${port}`))
