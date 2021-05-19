ee964a6a91d36347732db63d8fe3921461fee283
nafifurqon <muhammadnafi98@gmail.com>
2021-05-19 21:33:51 +0700

    update for deploy

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const port = 3000;
-const {
-    adminRouter,
-    gamesRouter,
-    userRouter,
-} = require('./routes');
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const port = process.env.PORT || 3000;
+const { adminRouter, gamesRouter, userRouter } = require("./routes");
 
-app.set('view engine', 'ejs');
-app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('static'));
-app.use('/sb-admin-2', express.static('node_modules/startbootstrap-sb-admin-2'));
+app.use(express.static("static"));
+app.use(
+  "/sb-admin-2",
+  express.static("node_modules/startbootstrap-sb-admin-2")
+);
 
-app.use('/', gamesRouter);
-app.use('/api/v1/users', userRouter)
-app.use('/admin', adminRouter);
+app.use("/", gamesRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/admin", adminRouter);
 
 app.use((error, req, res, next) => {
-    const status = error.errorStatus || 500;
-    const message = error.message;
-    const data = error.data;
-    console.log(error)
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+  console.log(error);
 
-    res.status(status).json({ message: message, data: data })
+  res.status(status).json({ message: message, data: data });
 });
 
-app.listen(port, () => console.log(`Binar challenge app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Binar challenge app listening at http://localhost:${port}`)
+);
